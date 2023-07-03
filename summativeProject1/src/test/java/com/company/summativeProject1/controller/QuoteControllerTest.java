a84faaec0670581c719497a44ba6d21ceb37bccb
catamtz3 <catamtz3@cs.washington.edu>
2023-07-03 08:31:23 -0700

    testing

--- summativeProject1/src/test/java/com/company/summativeProject1/controller/QuoteControllerTest.java
+++ summativeProject1/src/test/java/com/company/summativeProject1/controller/QuoteControllerTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.company.summativeProject1.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(QuoteController.class)
 class QuoteControllerTest {
-  
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void ShouldGetRandomQuote() throws Exception {
+        mockMvc.perform(
+                        get("/quote")
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
