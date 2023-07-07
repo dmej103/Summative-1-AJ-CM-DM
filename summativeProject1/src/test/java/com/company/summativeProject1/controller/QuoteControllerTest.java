d2a023fcc558f6498bde9e81701e9692c08adb61
catamtz3 <catamtz3@cs.washington.edu>
2023-07-06 17:09:56 -0700

    Fixed controller name

--- summativeProject1/src/test/java/com/company/summativeProject1/controller/QuoteControllerTest.java
+++ summativeProject1/src/test/java/com/company/summativeProject1/controller/QuoteControllerTest.java
@@ -1,5 +1,6 @@
-package com.company.summativeProject1.controllers;
+package com.company.summativeProject1.controller;
 
+import com.company.summativeProject1.controllers.QuoteController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
